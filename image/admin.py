ae272bf80b5801dd766ce50638f698c2088eee90
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-09-23 09:18:43 +0330

    create Image model and ImageAdmin

--- image/admin.py
+++ image/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from image.models import Image
 
-# Register your models here.
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    '''
+    image admin panel.
+    '''
+
+    list_display = ['owner', 'title']
+    list_filter = ['owner', 'update_at']
+    search_fields = ['title__icontains', 'description__icontains']
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+    fields = ['image', 'title', 'slug', 'description', 'users_like']
+    readonly_fields = ['owner']
+
+     # save owner
+    def save_model(self, request, obj, form, change):
+            # change owner field to owner requested
+            obj.owner = request.user
+            return super().save_model(request, obj, form, change)
--- image/models.py
+++ image/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 
-# Create your models here.
+
+User = get_user_model()
+
+class Image(models.Model):
+    '''
+    Image post.
+    '''
+
+    # initial information
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+    image = models.ImageField(upload_to='image/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    users_like = models.ManyToManyField(User, related_name='images_liked', blank=True)
+
+    # create at & update at
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-create_at'])
+        ]
+        ordering = ['-update_at', '-create_at']
+
+    # Save the slug with the title input parameter
+    def save(self, *args, **kwargs):
+        if not self.slug or self.slug != self.title:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super(Image, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
