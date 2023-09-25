274ac665c433a505ecd9d8c7cba05a8e939c4a0f
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-09-25 13:10:09 +0330

    create like/unlike system + create ImageLikeView +  update templates and style

--- image/urls.py
+++ image/urls.py
@@ -6,4 +6,5 @@ app_name = 'image'
 urlpatterns = [
     path('create/', views.ImageView.as_view(), name='image_create'),
     path('detail/<int:pk>/<slug:slug>/', views.ImageDetailView.as_view(), name='image_detail'),
+    path('like/', views.ImageLikeView.as_view(), name='image_like'),
 ]
--- image/views.py
+++ image/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib import messages
@@ -48,6 +49,31 @@ class ImageDetailView(View):
     
     def get(self, request, pk, slug):
         image = get_object_or_404(Image, id=pk, slug=slug)
+        users_like = image.users_like.all()
 
-        context = {'image': image}
+        context = {'image': image, 'users_like': users_like}
         return render(request, 'image/detail.html', context)
+
+
+class ImageLikeView(View):
+    '''
+    This class is for like or unlike post image.
+    '''
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            image_id = request.POST.get('id')
+            action = request.POST.get('action')
+            if image_id and action:
+                try:
+                    image = Image.objects.get(id=image_id)
+                    if action == 'like':
+                        image.users_like.add(request.user)
+                    else:
+                        image.users_like.remove(request.user)
+                    return JsonResponse({'status': 'ok'})
+                except Image.DoesNotExist:
+                    pass
+            return JsonResponse({'status': 'error'})
+        else:
+            return redirect('login')
