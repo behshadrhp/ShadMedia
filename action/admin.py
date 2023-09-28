8908e5e06d0eb99759a62237d9489dcddb865672
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-09-28 18:06:43 +0330

    create Action model + ActionAdmin

--- action/admin.py
+++ action/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Action)
+class ActionAdmin(admin.ModelAdmin):
+    '''
+    This class is for managing and reviews action model on admin panel.
+    '''
+
+    list_display = ['owner', 'verb', 'target', 'create_at']
+    list_filter = ['owner', 'create_at']    
+    search_fields = ['verb__icontains']
+    
